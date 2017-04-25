6cbff0db671561a26238fd6bfa53e3078b1c65d1
Lena <etarnovskaya@gmail.com>
2017-04-25 12:24:20 +0300

    added logInAdmin
    test result fail. org.openqa.selenium.ElementNotVisibleException:
    element not visible (Selenium не видит кнопку Логин). в чем может быть
    проблема?

--- sel-test/src/test/java/lena/lerning/selenium/FirstTest.java
+++ sel-test/src/test/java/lena/lerning/selenium/FirstTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleContains;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
@@ -19,6 +21,7 @@ public class FirstTest {
     @Before
     public void start() {
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }
 
@@ -29,6 +32,16 @@ public class FirstTest {
         driver.findElement(By.name("q")).sendKeys("webdriver");
         driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
         wait.until(titleContains("webdriver"));
+    }
+
+    @Test
+    public void logInAdmin(){
+        driver.get("http://localhost/litecart/admin/");
+
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+
     }
 
     @After
